6365531a9a91d1563015a4bbc567dd6d25214e85
Brian Henry <94brianhenry@gmail.com>
2018-05-04 13:27:33 -0700

    Update HomeController

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,15 +1,38 @@
-using System.Collections.Generic;
 using System;
+using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 using HairSalon.Models;
 
-public class HomeController : Controller
+namespace HairSalon
+{
+  public class HomeController : Controller
   {
-
-        [Route("/")]
-        public ActionResult Index()
-        {
-          return View();
-        }
-
+    [HttpGet("/")]
+    public ActionResult Index()
+    {
+      List <Client> all Clients = Clients.GetAll();
+      return View(allClients);
     }
+
+    [HttpGet("/Client/new")]
+    public ActionResult CreateForm()
+    {
+      return View();
+    }
+
+    [HttpPost("/Client")]
+    public ActionResult Create()
+    {
+      Client newClient = new Client(Request.Form["name"], Request.Form["email"], Request.Form["phoneNumber"], Request.Form["prefferedStylist"]);
+      List <Client> allClient = Client.GetAll();
+      return View("Index");
+    }
+
+    [HttpPost("Client/clear")]
+    public ActionResult Clear()
+    {
+      Client.ClearAll();
+      return View();
+    }
+  }
+}
